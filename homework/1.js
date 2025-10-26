af6ab587b1a23294b421b45023ad4bc74c410d27
xuezhennnn <88291141+xuezhennnn@users.noreply.github.com>
2025-10-26 17:54:48 +0100

    Update and rename lesson_1.js to homework/1.js

--- homework/1.js
+++ homework/1.js
@@ -2,14 +2,20 @@ document.body.innerHTML('Good morning');
 Uncaught TypeError: document.body.innerHTML is not a function
     <anonymous> debugger eval code:1
 debugger eval code:1:15
+
 document.body.innerHTML = 'Good morning';
 "Good morning"
+
 document.body.innerHTML = 'Zhang Xuezhen'
 "Zhang Xuezhen"
+
+// Math
 10 + 8 + 20
 38
 100 - 20 - 50 +200
 230
+
+// Output the blank
 document.body.innerHTML = ...;
 Uncaught SyntaxError: expected expression, got '...'
 debugger eval code:1:27
