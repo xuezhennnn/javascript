5f444d8b0b734e5e86cd4867044e59f54c1f6929
xuezhennnn <88291141+xuezhennnn@users.noreply.github.com>
2025-10-26 17:03:15 +0100

    Create lesson_1.js

---
+++ lesson_1.js
@@ -0,0 +1,19 @@
+document.body.innerHTML('Good morning');
+Uncaught TypeError: document.body.innerHTML is not a function
+    <anonymous> debugger eval code:1
+debugger eval code:1:15
+document.body.innerHTML = 'Good morning';
+"Good morning"
+document.body.innerHTML = 'Zhang Xuezhen'
+"Zhang Xuezhen"
+10 + 8 + 20
+38
+100 - 20 - 50 +200
+230
+document.body.innerHTML = ...;
+Uncaught SyntaxError: expected expression, got '...'
+debugger eval code:1:27
+document.body.innerHTML = ;
+Uncaught SyntaxError: expected expression, got ';'
+debugger eval code:1:27
+document.body.innerHTML = ''; 
