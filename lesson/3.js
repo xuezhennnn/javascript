30e2e9b0f99a2f7ffe29bf8f6859de60a544a385
xuezhennnn <88291141+xuezhennnn@users.noreply.github.com>
2025-10-27 19:54:12 +0100

    Create 3.js

---
+++ lesson/3.js
@@ -0,0 +1,32 @@
+'hello'
+alert('hello');
+
+'some' + 'text'
+'some' + 'more' + 'text'
+
+typeof 2
+typeof 'hello'
+
+'hello' + 3
+'$' + 20.95 + 7.99
+'$' + (20.95 + 7.99)
+'$' + (2095 + 799) / 100
+
+'Items (' + (1 + 1) + '): $' + (2095 + 799) / 100
+alert('Items (' + (1 + 1) + '): $' + (2095 + 799) / 100);
+
+"hello"
+"I'm learning JavaScript"
+
+// if u keep using ' for string, u can add \ to make it work
+'I\'m learning JavaScript'
+// \n means enter it change line
+alert('some\ntext');
+
+// template strings
+`hello`
+// ${}is the interpolation of the part
+`Items (${1 + 1}): $${(2095 + 799) / 100}`
+// multi line string
+`some
+text`
